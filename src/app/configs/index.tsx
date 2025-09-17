9a6469f45ff4dcd680e689d4506fbb19fa524305
Hazo Baikulov <spacehaz@gmail.com>
2025-09-17 17:00:16 +0200

    upates for adress of drop and token

--- src/app/configs/index.tsx
+++ src/app/configs/index.tsx
@@ -13,6 +13,6 @@ export const extensionDownloadLink = 'https://chromewebstore.google.com/detail/z
 export const xLink = 'https://x.com/bring_id'
 export const warpcastLink = 'https://warpcast.com/~/channel/bring'
 export const formLink = 'https://form.typeform.com/to/kmi5YYVY'
-export const dropAddress = '0x0C7aa84D1631fFaE57A2D0E7E56ee45d19ec04af'
+export const dropAddress = '0x0329EBFE50463B280aCDB953C7BE43835687deDE'
 export const pointsRequired = 10
 export const bringIDLink = 'https://bringid.org'
